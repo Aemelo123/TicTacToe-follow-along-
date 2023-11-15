15a3ac6d21b8719a84314b06b151154128f8f5d2
Aemelo123 <92276168+Aemelo123@users.noreply.github.com>
2023-11-15 16:21:52 -0500

    Fixed so it can determine the winner of the game

--- js/App.js
+++ js/App.js
@@ -1,55 +1,95 @@
-function App(){
+function App() {
     const winningCombos = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
-        [0, 3, 6], 
+        [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6]
-        ];
+    ];
 
-    const [board, setBoard] = React.useState(["", "", "", "", "", "", "", "", ""])
-    
-    let gameOver = false;
-    const [turn, setTurn] = React.useState('X')
-    let win;
+    const [board, setBoard] = React.useState(["", "", "", "", "", "", "", "", ""]);
+    const [turn, setTurn] = React.useState('X');
+    const [win, setWin] = React.useState(null);
+    const [gameOver, setGameOver] = React.useState(false);
+
+    React.useEffect(() => {
+        let whoWon = getWinner(board);
+        if (whoWon) {
+            setWin(whoWon);
+            setGameOver(true);
+        } else {
+            let isBoardFull = !board.includes('');
+            if (isBoardFull) {
+                setWin('T');
+                setGameOver(true);
+            } else {
+                setTurn(prevTurn => (prevTurn === 'X' ? 'O' : 'X'));
+            }
+        }
+    }, [board, gameOver]);
+
+    function getWinner() {
+        for (let i = 0; i < winningCombos.length; i++) {
+            const [a, b, c] = winningCombos[i];
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                return board[a]; // Return the winner (either 'X' or 'O')
+            }
+        }
+
+        // Check for a tie
+        if (!board.includes('')) {
+            return 'T'; // It's a tie
+        }
+
+        // No winner yet
+        return null;
+    }
 
     function handleTurn(event) {
-        let idx = event.target.id
-        if (gameOver == false) {
-            let newBoard = [...board]
-            newBoard[idx] = turn
-            setBoard(newBoard)
-            let nextTurn = turn === 'X' ? 'O' : 'X'
-            setTurn(nextTurn)
-            win = getWinner()
+        let idx = event.target.id;
+        if (!gameOver && !board[idx]) {
+            setBoard(prevBoard => {
+                let newBoard = [...prevBoard];
+                newBoard[idx] = turn;
+                return newBoard;
+            });
         }
-        // win = getWinner();
-        // render();
-    };
+    }
+
+    function Message() {
+        let message = win === 'T' ? 'That\'s a tie' : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+
+        return <h2>{message}</h2>;
+    }
+
+    function resetGame() {
+        setBoard(["", "", "", "", "", "", "", "", ""]);
+        setTurn('X');
+        setWin(null);
+        setGameOver(false);
+    }
+
 
     return (
         <div>
             <h1>Tic-Tac-Toe</h1>
 
-            <h2>It's {turn}'s turn!</h2>
-
-                <div class="flex-container flex-column">
-                    <div class="flex-container flex-wrap" id="board" onClick ={handleTurn}>
-                        {board.map((value, idx) => {
-                            return (
-                                <div class ="square" key ={idx} id={idx}>
-                                    {value}
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <button id="reset-button">reset</button>
+            <Message />
+            <div className="flex-container flex-column">
+                <div className="flex-container flex-wrap" id="board" onClick={handleTurn}>
+                    {board.map((value, idx) => (
+                        <div className="square" key={idx} id={idx}>
+                            {value}
+                        </div>
+                    ))}
                 </div>
+                <button id="reset-button" onClick={resetGame}>Reset</button>
+            </div>
         </div>
-    )
+    );
 }
 
-ReactDOM.render(<App />, root)
+ReactDOM.render(<App />, root);
